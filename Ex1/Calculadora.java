7e5010a6c3e67664da888db63e62b4b2ac378d15
Enzo0501 <165171939+Enzo0501@users.noreply.github.com>
2024-03-27 11:22:30 -0300

    Arquivos

---
+++ Ex1/Calculadora.java
@@ -0,0 +1,20 @@
+public class Calculadora {
+ 
+    public static float somar(float num1, float num2) {
+        return num1 + num2;
+    }
+
+   
+    public static float subtrair(float num1, float num2) {
+        return num1 - num2;
+    }
+
+    public static float multiplicar(float num1, float num2) {
+        return num1 * num2;
+    }
+
+    public static float dividir(float num1, float num2) {
+    return num1 / num2;
+             
+    }
+}
